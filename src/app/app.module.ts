f90eda5fd8531f9b33494247f52ecaa31c6d3096
baronrafael <baronrafael@hotmail.com>
2020-10-22 14:52:49 -0400

    feat: testing hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
@@ -13,6 +14,7 @@ import { LayoutComponent } from './components/layout/layout.component';
   imports: [
     BrowserModule,
     CoreModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
